eb5468b0112f122e45bac0e4ff0616791b269dd5
Ben Henderson <benh090704@gmail.com>
2018-08-14 10:35:29 +0100

    Added different hover effects to buttons and QA

--- Javascript/nav.js
+++ Javascript/nav.js
@@ -51,28 +51,4 @@ function ShowMessage() {
     // After 3 seconds, remove the show class from DIV
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1000);
 }
-
-    // Button Euro Capitals
-$("#btnec").click(()=>{
-    if(shuffled){ window.location.replace("./random_euro_capitals.html");}else{
-    window.location.replace("./euro_capitals.html");
-    }
-});
-    
-// Button Asia Capitals
-$("#btnac").click(()=>{
-    if(shuffled){  window.location.replace("./random_asia_capitals.html");}else{
-    window.location.replace("./asia_capitals.html");
-    }
-});
-    
-// Button Euro Languages
-$("#btnel").click(()=>{
-    if(shuffled){  window.location.replace("./random_euro_languages.html");}else{
-window.location.replace("./euro_languages.html");
-    }
-});
-
-
-
 });
